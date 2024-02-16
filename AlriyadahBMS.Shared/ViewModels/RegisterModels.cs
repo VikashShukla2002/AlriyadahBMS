1b1e7f0a7c0be11b75a504eea6b5665c4bed31c5
admin <admin@TO28>
2024-02-16 18:06:01 +0530

    added ui for registration page

---
+++ AlriyadahBMS.Shared/ViewModels/RegisterModels.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlriyadahBMS.Shared.ViewModels
+{
+    public class RegisterModels
+
+    { 
+        // general info
+
+        public string str_First_Name { get; set; }
+        public string str_Middle_Name { get; set; }
+        public string str_Last_Name { get; set; }
+        public string str_Cell_Phone { get; set; }
+        public string str_Email { get; set; }
+        public int Hijri_Day { get; set; }
+        public int Hijri_Month { get; set; }
+        public int Hijri_Year { get; set; }
+
+        // Access credentials
+        public string str_Password { get; set; }
+        public string str_NationalID_Iqama { get; set; }
+
+
+        // Driving status 
+        public bool IsDrivingexperience { get; set; }
+        public int intDrivinglicenseType { get; set; }
+        public string AbsherApptNbr { get; set; }
+        public string? Absherphoto { get; set; }
+
+
+        // emergency contact
+        public string str_Emergency_Contact_Name { get; set; }
+        public string str_Emergency_Contact_Phone { get; set; }
+        public string str_Emergency_Contact_Relation { get; set; }
+
+
+
+
+    }
+}
