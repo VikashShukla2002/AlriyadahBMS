54b30fa642cca0e6861a0fc6e129c4be6c7671f1
admin <admin@TO28>
2024-02-26 18:19:29 +0530

    added

--- AlriyadahBMS.Shared/ViewModels/EwLanguage.cs
+++ AlriyadahBMS.Shared/ViewModels/EwLanguage.cs
@@ -14,13 +14,39 @@ namespace AlriyadahBMS.Shared.ViewModels
         public string Description { get; set; } = null!;
         public string Author { get; set; } = null!;
         public string Date { get; set; } = null!;
-
-        public IEnumerable<Phrase> Phrases { get; set; } = null!;
+        public IEnumerable<Phrase> Global { get; set; } = null!;
+        public Project Project { get; set; } = null!;
     }
+
     public class Phrase
     {
         required public string Id { get; set; }
-        required public string Value { get; set; } 
+        required public string Value { get; set; }
     }
 
+    public class Project
+    {
+        public IEnumerable<Phrase> Phrases { get; set; } = null!;
+        public IEnumerable<Menu> Menus { get; set; } = null!;
+        public IEnumerable<Table> Tables { get; set; } = null!;
+    }
+
+    public class Menu
+    {
+        public Phrase Phrase { get; set; } = null!;
+        required public string Id { get; set; }
+    }
+
+    public class Table
+    {
+        required public string Id { get; set; }
+        public IEnumerable<Phrase> Phrases { get; set; } = null!;
+        public IEnumerable<Field> Fields { get; set; } = null!;
+    }
+
+    public class Field
+    {
+        required public string Id { get; set; }
+        public IEnumerable<Phrase> Phrases { get; set; } = null!;
+    }
 }
--- AlriyadahBMS/Services/LocalizationService.cs
+++ AlriyadahBMS/Services/LocalizationService.cs
@@ -20,7 +20,7 @@ namespace AlriyadahBMS.Services
         static LocalizationService()
         {
             //var path = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "wwwroot","language");
-                var path = System.IO.Path.Combine(AppContext.BaseDirectory, "wwwroot", "language");
+            var path = System.IO.Path.Combine(AppContext.BaseDirectory, "wwwroot", "language");
             var files = System.IO.Directory.GetFiles(path);
 
             foreach (var file in files)
@@ -38,14 +38,57 @@ namespace AlriyadahBMS.Services
                 //    Id = phrase.Attribute("id")!.Value,
                 //    Value = phrase.Attribute("value")!.Value
                 //});
-                var childElements = languageElement.Elements("");
-                var descendantElements = childElements.ToList();
+                //var globalChildElements = languageElement.Elements("global");
+                //var descendantElements = globalChildElements.ToList();
 
-                var phrases = doc.Descendants("global").Elements("phrase").Select(phrase => new Phrase()
+
+
+                var globalPhrases = doc.Descendants("global").Elements("phrase").Select(phrase => new Phrase()
                 {
                     Id = phrase.Attribute("id")!.Value,
                     Value = phrase.Attribute("value")!.Value
                 });
+
+                var project = new Project();
+
+                project.Phrases = doc.Descendants("project").Elements("phrase").Select(phrase => new Phrase()
+                {
+                    Id = phrase.Attribute("id")!.Value,
+                    Value = phrase.Attribute("value")!.Value
+                });
+
+
+                project.Menus = doc.Descendants("project").Elements("menu").Select(menu => new Menu
+                {
+                    Id = menu.Attribute("id")?.Value ?? "",
+                    Phrase = new Phrase
+                    {
+                        Id = menu.Element("phrase")?.Attribute("id")?.Value ?? "",
+                        Value = menu.Element("phrase")?.Attribute("value")?.Value ?? ""
+                    }
+                });
+
+
+                project.Tables = doc.Descendants("project").Elements("table").Select(table => new Table
+                {
+                    Id = table.Attribute("id")?.Value ?? "",
+                    Phrases = table.Elements("phrase").Select(phrase => new Phrase()
+                    {
+                        Id = phrase.Attribute("id")!.Value,
+                        Value = phrase.Attribute("value")!.Value
+                    }),
+
+                    Fields = table.Elements("field").Select(field => new Field
+                    {
+                        Id = field.Attribute("id")?.Value ?? "",
+                        Phrases = field.Elements("phrase").Select(phrase => new Phrase
+                        {
+                            Id = phrase.Attribute("id")?.Value ?? "",
+                            Value = phrase.Attribute("value")?.Value ?? ""
+                        }).ToList()
+                    }).ToList()
+                });
+
 
 
                 EwLanguages.Add(new EwLanguage()
@@ -56,8 +99,13 @@ namespace AlriyadahBMS.Services
                     Description = description,
                     Author = author,
                     Date = date,
-                    Phrases = phrases
+                    Global = globalPhrases,
+                    Project = project,
                 });
+
+
+
+
             }
         }
 
@@ -67,9 +115,28 @@ namespace AlriyadahBMS.Services
             {
                 SelectedLanguage = EwLanguages.First();
             }
-            var value = SelectedLanguage.Phrases.FirstOrDefault(a => a.Id.Equals(id.Trim(), StringComparison.CurrentCultureIgnoreCase))?.Value;
+            var value = SelectedLanguage.Global.FirstOrDefault(a => a.Id.Equals(id.Trim(), StringComparison.CurrentCultureIgnoreCase))?.Value;
             return value;
         }
+
+
+        public string? Phrase(string tableId, string phraseId)
+        {
+            var table = SelectedLanguage.Project.Tables.FirstOrDefault(t => t.Id == tableId);
+            if (table != null)
+            {
+
+
+                var fieldPhrase = table.Phrases.FirstOrDefault(p => p.Id == phraseId);
+                if (fieldPhrase != null)
+                {
+                    return fieldPhrase.Value;
+                }
+
+            }
+            return null;
+        }
+
     }
 }
 
